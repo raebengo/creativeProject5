cf8a81b94d008993ab4910bcfb115746482aa277
Rachel Bengochea <rachel.bengochea@byu.net>
2018-04-18 12:52:50 -0600

    stuff

--- migrations/20180320114412_tweets.js
+++ migrations/20180320114412_tweets.js
@@ -1,4 +1,12 @@
-
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('tweets', function(table) {
+      table.increments('id').primary();
+      table.string('tweet');
+      table.dateTime('created');
+      table.integer('user_id').unsigned().notNullable().references('id').inTable('users');
+    }),
+  ]);
 
 exports.down = function(knex, Promise) {
   return Promise.all([
